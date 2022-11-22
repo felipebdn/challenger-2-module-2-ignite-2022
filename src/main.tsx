3a12d393c5618107c7d3e2dfb96dee794b4d5386
Felipe Barbosa <71861370+felipebdn@users.noreply.github.com>
2022-11-22 18:37:09 -0300

    clonagem do modelo github

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import { App } from './App'
+import { GlobalStyle } from './Global/GlobalStyle'
+import { defaultTheme } from './Global/themes/default'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
