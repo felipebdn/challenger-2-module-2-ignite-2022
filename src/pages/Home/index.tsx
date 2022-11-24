d100ea8af18276e68dc91d9226138d3ffec20345
Felipe Barbosa <71861370+felipebdn@users.noreply.github.com>
2022-11-24 16:59:36 -0300

    165924112022

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,47 +1,31 @@
-import { Atributos, HalfAtributes, HalfSection, HomeContainer, SectionApresentation, Titles } from "./styles";
-import { ShoppingCart } from 'phosphor-react'
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+import { HomeContainer, TitleHome } from './styles'
+// import { ShoppingCart } from 'phosphor-react'
 export function Home() {
   return (
     <HomeContainer>
-      <SectionApresentation>
-        <HalfSection>
-          <Titles>
-            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-            <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
-          </Titles>
-          <Atributos>
-            <HalfAtributes>
-              <div>
-                <div>
-                  <ShoppingCart size={16} weight="fill" />
-                </div>
-                <span>Compra simples e segura</span>
-              </div>
-              <div>
-                <div>
-                  <ShoppingCart size={16} weight="fill" />
-                </div>
-                <span>Compra simples e segura</span>
-              </div>
-            </HalfAtributes>
-            <HalfAtributes>
-              <div>
-                <div>
-                  <ShoppingCart size={16} weight="fill" />
-                </div>
-                <span>Compra simples e segura</span>
-              </div>
-              <div>
-                <div>
-                  <ShoppingCart size={16} weight="fill" />
-                </div>
-                <span>Compra simples e segura</span>
-              </div>
-            </HalfAtributes>
-          </Atributos>
-        </HalfSection>
-        <div>imagem</div>
-      </SectionApresentation>
+      <main>
+        <TitleHome>
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <p>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </p>
+        </TitleHome>
+        <div>
+          <ShoppingCart size={16} weight="fill" />
+          <span>Compra simples e segura</span>
+          <Timer size={16} weight="fill" />
+          <span>Entrega rápida e rastreada</span>
+          <Package size={16} weight="fill" />
+          <span>Embalagem mantém o café intacto</span>
+          <Coffee size={16} weight="fill" />
+          <span>O café chega fresquinho até você</span>
+        </div>
+      </main>
+      <aside>
+        <img src="./assets/coffeeHome.svg" alt="" />
+      </aside>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,52 +4,43 @@ export const HomeContainer = styled.div`
   width: 100vw;
   padding: 0 15rem;
   background: ${(props) => props.theme.base['base-hover']};
+  display: flex;
+  flex-direction: row;
+
+  main {
+    display: flex;
+    flex-direction: column;
+    gap: 4.125rem;
+  }
+
+  main,
+  aside {
+    width: 50%;
+  }
+
+  aside:nth-child(2) {
+    display: flex;
+    justify-content: center;
+  }
 `
-export const SectionApresentation = styled.section`
+
+export const TitleHome = styled.div`
   width: 100%;
-  display: flex;
-`
-export const HalfSection = styled.div`
-  width: 50%;
-`
-
-export const Titles = styled.div`
-  display: flex;
-  flex-direction: column;
   gap: 1rem;
-
   h1 {
     font-family: 'Baloo 2';
     font-style: normal;
     font-weight: 800;
-    font-size: 48px;
+    font-size: 3rem;
     line-height: 130%;
     color: ${(props) => props.theme.base['base-title']};
   }
   p {
     font-family: 'Roboto';
     font-style: normal;
-    font-weight: 400;
-    font-size: 20px;
+    font-weight: 500;
+    font-size: 1.25rem;
     line-height: 130%;
     color: ${(props) => props.theme.base['base-subtitle']};
-  }
-`
-export const Atributos = styled.div`
-  width: 100%;
-  display: flex;
-`
-
-export const HalfAtributes = styled.div`
-  width: 50%;
-
-  div {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-
-    div {
-      padding: 0.5rem;
-    }
   }
 `
